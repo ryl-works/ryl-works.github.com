4c3f44468e45383192e4f04532fa51afecc3af90
ryl <380034125@qq.com>
2017-08-20 18:26:16 +0800

    step14-comment评价功能ryl

---
+++ react/react-ryl-demo/step14-order-comment/app/containers/Home/subpage/Ad.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import PureRenderMixin from 'react-addons-pure-render-mixin'
+import HomeAd from '../../../components/HomeAd/'
+import { getAdData } from '../../../fetch/home/home'
+
+//静态数据
+// import AdData from '../../../../mock/home/ad.js'
+
+class Ad extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+        this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+        this.state = {
+          data: []
+        }
+    }
+    render() {
+        return (
+            <div>
+            {
+              this.state.data.length
+              ? <HomeAd data={this.state.data} />
+              : <div>{/*加载中*/}</div>
+            }
+            </div>
+        )
+    }
+    componentDidMount(){
+      //静态数据
+      // const data = AdData;
+      // console.log(data);
+      //   if(data.length){
+      //     this.setState({
+      //       data: data
+      //     })
+      //   }
+      //获取广告数据
+      const  result = getAdData()
+      result.then(res => {
+        return res.json()
+      }).then(json => {
+        // 处理获取的数据
+        const data = json
+        if(data.length){
+          this.setState({
+            data: data
+          })
+        }
+      }).catch(ex => {
+        // 发生错误
+        if(__DEV__){
+          console.error('首页广告模块获取数据报错,',ex.message);
+        }
+      })
+    }
+}
+
+export default Ad
